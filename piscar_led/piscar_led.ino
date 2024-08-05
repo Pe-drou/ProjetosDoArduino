314653822ff5dbe45d565e2f110d5eedb6033198
Pedro <phsr300408@gmail.com>
2024-08-05 15:08:58 -0300

    comentários no código

--- piscar_led/piscar_led.ino
+++ piscar_led/piscar_led.ino
@@ -1,10 +1,19 @@
+//pisca um led na porta 2
 void setup() {
+  //define o modo da porta
 pinMode(2, OUTPUT);
 }
 
 void loop() {
   digitalWrite(2, HIGH);
+  //ascende o led
+  
   delay(1000);
+  //espera 1 segundo
+  
   digitalWrite(2, LOW);
+  //apaga o led
+  
   delay(1000);
+  //espera mais 1 segundo
 }
